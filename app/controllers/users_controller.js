0e497f61192b817b2a9dccf828e856580b877392
Shaiane Boesing Rodrigues Borges <shaianeboesingrb@gmail.com>
2022-11-27 12:32:00 -0300

    :sparkles: feat: finalizando CRUD de usuários

--- app/controllers/users_controller.js
+++ app/controllers/users_controller.js
@@ -2,28 +2,83 @@ const User = require('../models/User');
 const md5 = require('md5');
 
 exports.index = async function(req, res) {
-    let users = await User.find()
-    res.status(201).json({data: users})
+    let users = await User.find();
+    res.status(201).json({data: users});
+};
+
+exports.show = async function(req, res) {
+    let id = req.params.id;
+    let user = await User.find({_id: id});
+    res.status(201).json({data: user});
+};
+
+exports.create = function(req, res) {
+    if (typeof __current_user !== 'undefined') {
+        res.sendFile(__basedir + '/web/views/users/form.html');
+    } else {
+        res.sendFile(__basedir + '/web/views/login/index.html');
+    }
 };
 
 exports.store = async function(req, res) {
-    const {name, email} = req.body;
-    const password = md5(req.body.password);
-    let user = await User.find({"email": email});
-    if(!user) {
-        const user = {
-            name, 
-            email, 
-            password
-        };
-        await User.create(user)
-        res.status(201).json({message: 'Usuário Criado com Sucesso'})    
-    } else  {
-        res.status(409).json({message: 'Usuário Já Existe'})    
+    if (validateUser(req)) {
+        const {name, email} = req.body;
+        const password = md5(req.body.password);
+        let user = await User.findOne({"email": email});
+        if(!user) {
+            const data = {
+                name, 
+                email, 
+                password
+            };
+            let user = await User.create(data);
+            res.status(201).json({message: 'Usuário Criado com Sucesso', user: user});  
+        } else  {
+            res.status(409).json({message: 'Usuário Já Existe'});
+        }    
+    } else {
+        res.status(400).json({message: 'Valores inválidos'});
+    }
+};
+
+exports.update = async function(req, res) {
+    if (validateUser(req)) {
+        const id = req.params.id;
+        const name = req.body.name;
+        const email = req.body.email;
+        let user = await User.findOneAndUpdate({"_id": id}, {name: name, email: email}, {new: true});  //new é para retornar o usuário com seus valores atualizados  
+        if(user) {
+            res.status(201).json({message: 'Usuário Atualizado com Sucesso', user: user});
+        } else  {
+            res.status(404).json({message: 'Usuário não encontrado'});
+        }
+    } else {
+        res.status(400).json({message: 'Valores inválidos'});
+    }
+};
+
+exports.destroy = async function(req, res) {
+    const id = req.params.id;
+    let user = await User.findOneAndRemove({_id: id});
+    if (user) {
+        res.status(204);
+    } else {
+        res.status(404).json({message: 'Usuário não encontrado'});
     }
 };
 
 exports.destroyAll = async function(req, res) {
-    await User.deleteMany({})
-    res.status(204).json({message: 'Usuários deletados com sucesso!'})
+    await User.deleteMany({});
+    res.status(204);
 };
+
+const validateUser = (req) => {
+    const {name, email} = req.body;
+    return  ((name.length > 3) && (validateEmail(email)));
+}
+
+const validateEmail = (email) => {
+    var pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/; 
+    return email.match(pattern);
+};
+  
--- app/routes/index.js
+++ app/routes/index.js
@@ -10,10 +10,10 @@ routes.post('/logout', session.logout);
 // Users
 routes.get('/users', users.index);
 routes.post('/users', users.store);
-// routes.get('/users/create', users.create);
-// routes.get('/users/:id/show', users.show);
-// routes.patch('/users/:id/update', users.update);
-//routes.delete('/users/:id/delete', users.destroy);
+routes.get('/users/create', users.create);
+routes.get('/users/:id', users.show);
+routes.patch('/users/:id', users.update);
+routes.delete('/users/:id', users.destroy);
 routes.delete('/users/delete/all', users.destroyAll);
 
 
