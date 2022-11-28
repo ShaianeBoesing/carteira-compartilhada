9255f2cfbd48b9dd434e042525b53f06bb87b340
Shaiane Boesing Rodrigues Borges <shaianeboesingrb@gmail.com>
2022-11-27 23:49:43 -0300

    :sparkles: feat: incluindo timestamps para criação de carteira

--- app/models/Wallet.js
+++ app/models/Wallet.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const Wallet = mongoose.model("Wallet", {
-    name: {type: String}
-});
+const walletSchema = new Schema(
+    { name: {type: String} }, 
+    { timestamps: true }
+);
+
+const Wallet = mongoose.model("Wallet", walletSchema);
 
 module.exports = Wallet
