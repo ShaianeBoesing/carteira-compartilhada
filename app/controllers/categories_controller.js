6051e6a982b69aa017c630c418f8d8294509d64e
marvin10036 <marvin100036@gmail.com>
2022-11-29 21:47:51 -0300

    Categoria testada

--- app/controllers/categories_controller.js
+++ app/controllers/categories_controller.js
@@ -19,14 +19,14 @@ exports.store = async function(req, res) {
     if (validateCategory(req)) {
         const name = (req.body.name).trim().toUpperCase();
         const type = req.body.type;
-        let category = await Category.findOne({"name": name});
-        if(!category) {
-            const data = {"name": name, "type": type};
-            let category = await category.create(data);
-            res.status(201).json({message: 'Categoria com Sucesso', category: category});  
-        } else  {
-            res.status(409).json({message: 'Não é possível criar duas categorias com o mesmo nome'});
-        }    
+        //let category = await Category.findOne({"name": name});
+        //if(!category) {
+        const data = {"name": name, "type": type};
+        let category = await Category.create(data);
+        res.status(201).json({message: 'Categoria criada com Sucesso', category: category});  
+        // } else  {
+        //    res.status(409).json({message: 'Não é possível criar duas categorias com o mesmo nome'});
+        // }    
     } else {
         res.status(400).json({message: 'Valores inválidos'});
     }
@@ -35,8 +35,8 @@ exports.store = async function(req, res) {
 exports.update = async function(req, res){
     if (validateCategory(req)){
         const id = req.params.id;
-        const name = req.body.name;
-        const type = req.body.type;
+        const name = req.body.name.trim().toUpperCase();
+        const type = req.body.type
         let category = await Category.findOneAndUpdate({"_id": id}, {name: name, type: type}, {new: true})
         if (category){
             res.status(201).json({message: 'Categoria atualizada com sucesso', category: category});
