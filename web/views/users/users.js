11c57320e443a6a6eaab5749c5f7167c2ca9f17f
João Vitor Duarte Domingos <jv.dd.senai.sc@gmail.com>
2022-12-11 19:42:09 -0300

    testes

--- web/views/users/users.js
+++ web/views/users/users.js
@@ -3,14 +3,26 @@ const username = document.querySelector('#username');
 const password = document.querySelector('#password');
 const repeatPassword = document.querySelector('#repeat-password');
 
+const url = '../controllers/users_controller/';
+
+let data = {
+    'name': username.value,
+    'email': email.value
+}
+
+let fetchData = {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: new Headers({
+        'Content-Type': 'application/json; charset=UTF-8'
+    })
+}
+
+fetch(url, fetchData)
+    .then(function () {
+        // Handle response you get from the API
+    });
+
 const registrar = () => {
-    
-    console.log(email.value)
-    fetch('../models/User')
-        .then(function () {
-            // handle the response
-        })
-        .catch(function () {
-            // handle the error
-        });
+
 }
